1bf9e4f1950decbc1fda502bc9faf4ce3493a630
hanrud <hania.rudy@gmail.com>
2017-08-23 21:12:00 +0200

    add max level field to model; changes styles

--- resv/models.py
+++ resv/models.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 from decimal import Decimal
 
+
 # Create your models here.
 class Reservoir(models.Model):
     name = models.CharField(max_length=30)
     lat = models.DecimalField(decimal_places=6, max_digits=8, default=Decimal('0.000000'))
     long = models.DecimalField(decimal_places=6, max_digits=9, default=Decimal('0.000000'))
+    maxLevel = models.FloatField(default=0.00)
     addDate = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
